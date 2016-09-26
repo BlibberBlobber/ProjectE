c61750603448149acfdb5f9e216d17e174e4e233
BlibberBlobber <Mortenvorborg@hotmail.com>
2016-09-26 13:10:09 +0200

    yabba

--- Nemt/src/Euler2.java
+++ Nemt/src/Euler2.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 /*
- * By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+ * By considering the terms in the Fibonacci sequence 
+ * whose values do not exceed four million, find the 
+ * sum of the even-valued terms.
  */
 
 public class Euler2 {
