2ca4d28c7069eac9fc2ef93c7d5768e06d823721
BlibberBlobber <Mortenvorborg@hotmail.com>
2016-09-26 12:44:30 +0200

    Ham der Brian Nielsen, han er Brian!

--- Nemt/src/Hallo.java
+++ Nemt/src/Hallo.java
@@ -1,22 +1,11 @@
 
 public class Hallo {
 	public static void main(String[] args) {
-		System.out.println("hej");
-		System.out.println("fedt fedt fedt");
-		//yolo
 		
 		
-		// Du er en **************
-		
-		System.out.println("yolo");
-		System.out.println("yolo");
+		String Jytte = "Brian";
 		
 		
-		int dum = 9001;
-		dum = dum + 3*dum;
-		System.out.println(dum);
-		dum = 3*4 + dum;
-		dum = 4*dum;
 		
 	}
 }
