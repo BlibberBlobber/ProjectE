642f0fe1e60186494a59dbd26291b24d17c957bd
madso <madso@MADS-LAPTOP.eduroam.wireless.dtu.dk>
2016-10-03 10:46:23 +0200

    Hermed øvelsen

--- Nemt/src/Euler4.java
+++ Nemt/src/Euler4.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 //A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 //
 //Find the largest palindrome made from the product of two 3-digit numbers.
@@ -5,8 +8,34 @@
 public class Euler4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner primtal = new Scanner(System.in);
+		int[] femPrimtal;
+		femPrimtal = new int[5];
+		System.out.println("Indtast fem heltal");
+		femPrimtal[0] = primtal.nextInt();
+		femPrimtal[1] = primtal.nextInt();
+		femPrimtal[2] = primtal.nextInt();
+		femPrimtal[3] = primtal.nextInt();
+		femPrimtal[4] = primtal.nextInt();
+		primtal.close();
+		
+		System.out.println("Vi undersøger følgende tal: " + Arrays.toString(femPrimtal) + "\n");
+		
+		int p = 2;
+		for (int i =0; i < 5;) {
+		while ( p <= femPrimtal[i] ){
+			if (femPrimtal[i]%p==0) {
+				femPrimtal[i]/=p;
+				System.out.println(p + " var en prim-faktor i " + femPrimtal[i]);
+			}
+			else {
+					p++;
+			}
+			}
+			i++;
+		}
+		
+	}
 
 	}
 
-}
