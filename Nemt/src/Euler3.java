813d0c390e48373639e23656c4711610930623c5
madso <madso@MADS-LAPTOP.eduroam.wireless.dtu.dk>
2016-10-03 08:14:57 +0200

    Nu med Euler 3

--- Nemt/src/Euler3.java
+++ Nemt/src/Euler3.java
@@ -1,4 +1,17 @@
 
+
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?*/
+
 public class Euler3 {
+	
+public static void main (String[] args) {
+	int a = 5;
+	int b = 7;
+	int c = 13;
+	int d = 29;
+	System.out.println(a*b*c*d);
+	
+}
 
 }
