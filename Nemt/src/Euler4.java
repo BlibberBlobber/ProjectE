6711d17ea845be0c8e255826d6698298fe96eec3
madso <madso@192.168.1.106>
2016-10-23 17:57:14 +0200

    Så fik jeg gjort det lidt pænere

--- Nemt/src/Euler4.java
+++ Nemt/src/Euler4.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Euler4 {
 
 	public static void main(String[] args) {
-		System.out.print("Indtast et trecifret tal, som skal undersøges for palindromer: ");
+		/*System.out.print("Indtast et trecifret tal, som skal undersøges for palindromer: ");
 		Scanner input = new Scanner(System.in);
 		long factor = input.nextLong();
 		
@@ -26,18 +26,19 @@ public class Euler4 {
 				System.out.println(factor + " * " + i + " = " + counter + " er et palindrom.");
 				}
 			  i--;
-		}	
+		}	*/
 		long palindrome = 0;
 		long reverse = 0;
 		long remainder;
-		factor = 111;
-		i = 999;
-		int k = 0;
-		ArrayList pal = new ArrayList();
+		long factor = 111;
+		long i = 999;
+		
+		ArrayList<Long> pal = new ArrayList<Long>();
+		
 		for (factor = 111; factor < i; factor++){
 			//System.out.println("Vi undersøger multipli af "+ factor +" og " + i +" (og nedefter)");
 		while (i > 100 ){  // Dette loop tester vha MATH (dobbelt-loop-whoop-whoop)
-			counter = factor * i;
+			long counter = factor * i;
 			palindrome = counter;
 			remainder = 0;
 			reverse = 0;
@@ -50,16 +51,18 @@ public class Euler4 {
 			if ( reverse == counter){
 				//System.out.println(factor + " * " + i + " = " + counter + " er et palindrom.");
 				pal.add(counter);
-				k++;
+				
 			}
 			i--;
 		}
 		i = 999;
 		}
-		long[] a = new long[k];
-		pal.toArray(); 
 		
-		System.out.println(a.toString());
+		Collections.sort(pal);
+		
+		long svar2 = pal.get(pal.size()-1);
+		
+		System.out.println("Det største palindrome tal er: " + svar2);
 	}
 	}
 
