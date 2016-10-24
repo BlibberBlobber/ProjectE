87c339d5d894752afbca6b0f8e2105e54d31ea6e
madso <madso@192.168.1.106>
2016-10-24 20:47:55 +0200

    Meh

--- Nemt/src/Euler5.java
+++ Nemt/src/Euler5.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
  * 
@@ -9,13 +11,21 @@ public class Euler5 {
 	public static void main(String[] args){
 		int n=1;
 		int i=1;
-		
-		for (n = 1; n<=20;){
-			
+		ArrayList<Integer> result = new ArrayList();
+	while (i>20 && 1000>i) {
+		n = 1;
+		while (n<=20){
 			if (i%n==0) n++;
 			if (!(i%n==0)) i++;
-			if (n==20) System.out.println(i);
+			if (n==20){
+				System.out.println(i);
+				result.add(i);
+			}
+		} 
+		i++;
 		}
+	
+		
 		
 		
 		
