567cf45b2831d688fd51be2c79b9ce017998e464
madso <madso@MADS-LAPTOP.eduroam.wireless.dtu.dk>
2016-10-26 10:49:00 +0200

    Med tekst.

--- Nemt/src/Euler6.java
+++ Nemt/src/Euler6.java
@@ -1,4 +1,13 @@
-
+/* Problem 6:
+ * The sum of the squares of the first ten natural numbers is,
+ * 385
+ * The square of the sum of the first ten natural numbers is,
+ * 3025
+ * 
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+ * 
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. 
+ */
 public class Euler6 {
 
 	public static void main(String[] args) {
