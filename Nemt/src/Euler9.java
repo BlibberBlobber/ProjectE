f15aa8b6f575948a0790b71e9feb3d1b1586b213
Madsost <m@madsostergaard.dk>
2017-03-28 12:42:38 +0200

    Hej Morten

---
+++ Nemt/src/Euler9.java
@@ -0,0 +1,19 @@
+/*
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ * 
+ * Dickson's metode: x = r+s, y = r+t, z = r+s+t
+ * vælg r s og t så r^2 = 2st 
+ * 
+ * c = 1000-a-b
+ * a^2 + b^2 = (1000-a-b)^2
+ */
+
+public class Euler9 {
+	public static void main(String[] args) {
+		boolean test = false;
+
+		
+		//System.out.println(svar);
+	}
+}
